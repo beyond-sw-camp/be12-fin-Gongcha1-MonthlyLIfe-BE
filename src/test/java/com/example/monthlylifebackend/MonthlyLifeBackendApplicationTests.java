bc845b89691bc54e1f82d93288f509e5dee06e5b
celarim <k6jun@kookmin.ac.kr>
2025-03-28 12:49:37 +0900

    [Init] Project init

---
+++ src/test/java/com/example/monthlylifebackend/MonthlyLifeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monthlylifebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonthlyLifeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
