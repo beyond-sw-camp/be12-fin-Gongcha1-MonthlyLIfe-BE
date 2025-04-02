b9e4414ef6948895bca80bb0c8dfb42b0ec2cd39
wkdlrn <kkjjgg97@gmail.com>
2025-04-02 18:19:59 +0900

    [Feat] fix
    고침

--- src/main/java/com/example/monthlylifebackend/product/repository/ProductRepository.java
+++ src/main/java/com/example/monthlylifebackend/product/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.example.monthlylifebackend.product.repository;
 
 
+import com.example.monthlylifebackend.product.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
