e980c9fbd277ce244d10d6fad162c17021a0c2a8
celarim <k6jun@kookmin.ac.kr>
2025-04-10 09:34:19 +0900

    [Docs] Mapper 사용 예시 exam에 추가
    파이팅

---
+++ src/main/java/com/example/monthlylifebackend/exam/ExamMapper.java
@@ -0,0 +1,11 @@
+package com.example.monthlylifebackend.exam;
+
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ExamMapper {
+    //SampleDTO toDTO(SampleEntity e);
+
+    //SampleEntity toEntity(SampleDTO dto);
+}
