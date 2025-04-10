ae1314816491a0910ef3d335120bf977c8537159
wkdlrn <kkjjgg97@gmail.com>
2025-04-10 16:35:45 +0900

    [Feat] 상품 상세 조회 기능 추가

--- src/main/java/com/example/monthlylifebackend/product/mapper/ProductMapper.java
+++ src/main/java/com/example/monthlylifebackend/product/mapper/ProductMapper.java
@@ -1,6 +1,7 @@
 package com.example.monthlylifebackend.product.mapper;
 
 import com.example.monthlylifebackend.product.dto.req.PostProductRegisterReq;
+import com.example.monthlylifebackend.product.dto.res.GetProductDetailRes;
 import com.example.monthlylifebackend.product.dto.res.GetProductListRes;
 import com.example.monthlylifebackend.product.model.Product;
 import com.example.monthlylifebackend.product.model.ProductImage;
@@ -13,4 +14,6 @@ public interface ProductMapper {
     Product toEntity(PostProductRegisterReq dto);
 
     GetProductListRes toGetProductListRes(Product product);
+    // 상세 조회용
+    GetProductDetailRes toGetProductDetailRes(Product product);
 }
