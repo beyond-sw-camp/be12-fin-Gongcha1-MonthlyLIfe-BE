4a39dff321b3c61406ac2e499a1007263a8d1180
wkdlrn <kkjjgg97@gmail.com>
2025-04-10 16:19:22 +0900

    [Feat] 상품 목록 조회 로직 추가

--- src/main/java/com/example/monthlylifebackend/product/service/ProductService.java
+++ src/main/java/com/example/monthlylifebackend/product/service/ProductService.java
@@ -2,6 +2,7 @@ package com.example.monthlylifebackend.product.service;
 
 
 import com.example.monthlylifebackend.product.dto.req.PostProductRegisterReq;
+import com.example.monthlylifebackend.product.dto.res.GetProductListRes;
 import com.example.monthlylifebackend.product.mapper.ProductMapper;
 import com.example.monthlylifebackend.product.model.Product;
 import com.example.monthlylifebackend.product.repository.ProductRepository;
@@ -9,6 +10,8 @@ import com.example.monthlylifebackend.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +22,13 @@ public class ProductService {
 
     public Long registerProduct(PostProductRegisterReq dto) {
          return productRepository.save(productMapper.toEntity(dto)).getIdx();
+    }
+
+    public List<GetProductListRes> getProductList() {
+        List<Product> products = productRepository.findAll();
+        return products.stream()
+                .map(productMapper::toGetProductListRes)
+                .toList();
     }
 
 }
