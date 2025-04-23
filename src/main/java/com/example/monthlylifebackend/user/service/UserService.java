ce6cbe17f0124247d57d1eeed9f433e0e58af348
= <sjhyun0926@gmail.com>
2025-04-23 11:55:53 +0900

    [Feat] 사용자 관리 기능 구현 - 사용자 관리 기능 구현

--- src/main/java/com/example/monthlylifebackend/user/service/UserService.java
+++ src/main/java/com/example/monthlylifebackend/user/service/UserService.java
@@ -3,10 +3,15 @@ package com.example.monthlylifebackend.user.service;
 
 import com.example.monthlylifebackend.common.exception.handler.UserHandler;
 import com.example.monthlylifebackend.user.dto.req.PostSignupReq;
+import com.example.monthlylifebackend.user.dto.res.GetAdminUserRes;
 import com.example.monthlylifebackend.user.mapper.UserMapper;
 import com.example.monthlylifebackend.user.model.User;
 import com.example.monthlylifebackend.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,7 +23,6 @@ import static com.example.monthlylifebackend.common.code.status.ErrorStatus._NOT
 
 @Service
 @RequiredArgsConstructor
- 
 public class UserService {
     private final UserRepository userRepository;
 
@@ -26,18 +30,30 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
 
     public User getUser(String id) {
-        return userRepository.findById(id).orElseThrow( () -> new UserHandler(_NOT_FOUND_USER) );
+        return userRepository.findById(id).orElseThrow(() -> new UserHandler(_NOT_FOUND_USER));
     }
 
     @Transactional
     public String signup(PostSignupReq dto) {
         // TODO : 동시에 유저를 회원가입시키면 충돌나서 락을 걸거나 다른 로직을 도입해야 한다.
         Optional<User> check = userRepository.findById(dto.getId());
-        if(check.isPresent()) throw new UserHandler(_DUPLICATED_USER);
+        if (check.isPresent()) throw new UserHandler(_DUPLICATED_USER);
 
         User user = userMapper.toEntity(dto);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
         return user.getId();
     }
+
+
+    //관리자 사용자 조회
+    @Transactional(readOnly = true)
+    public Page<GetAdminUserRes> getAdminUserList(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by("createdAt").descending());
+
+        return userRepository.findAll(pageable)
+                .map(userMapper::toUserInfoRes);
+    }
 }
+
+
