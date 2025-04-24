347aa2d31639a5c623120d5f03587f199d69fc29
celarim <k6jun@kookmin.ac.kr>
2025-04-24 10:10:48 +0900

    [Feat] 유저 도메인에 우편번호 추가

--- src/main/java/com/example/monthlylifebackend/user/dto/res/GetUserDetailRes.java
+++ src/main/java/com/example/monthlylifebackend/user/dto/res/GetUserDetailRes.java
@@ -1,6 +1,7 @@
 package com.example.monthlylifebackend.user.dto.res;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,6 +23,8 @@ public class GetUserDetailRes {
     private LocalDate birth;
     @Schema(description = "유저 전화번호(숫자 9 ~ 11자)",  example = "01012345678")
     private String phoneNumber;;
+    @Schema(description = "유저 우편번호(숫자 5자)", example = "07060")
+    private String postalCode;
     @Schema(description = "유저 기본 주소(100자 이내)", example = "서울시 동작구 보라매로 87")
     private String address1;
     @Schema(description = "유저 상세 주소(100자 이내)", example = "4층 12기 강의실")
